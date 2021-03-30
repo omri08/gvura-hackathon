4c517f02c2c0e4b100d3e9f43b573b47029d29e7
AmitMizrahi123 <amitmizrahi231055@gmail.com>
2021-03-30 22:55:30 +0300

    create registration and signIn API

---
+++ services/firebase.js
@@ -0,0 +1,38 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import { Alert } from 'react-native';
+
+export async function registration(email, password, lastName, firstName) {
+  try {
+    await firebase.auth().createUserWithEmailAndPassword(email, password);
+    const currentUser = firebase.auth().currentUser;
+
+    const db = firebase.firestore();
+    db.collection('users').doc(currentUser.uid).set({
+      email: currentUser.email,
+      lastName: lastName,
+      firstName: firstName,
+    });
+
+    db.collection('profiles').doc(currentUser.uid).set({
+      isAvailable: false,
+      location: null,
+      score: 0,
+      preference: null,
+    });
+    Alert.alert('Registration complete');
+  } catch (err) {
+    Alert.alert('There is something wrong!!!!', err.message);
+  }
+}
+
+export async function signIn(email, password) {
+  try {
+   await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
+    Alert.alert('SignIn complete');
+  } catch (err) {
+    Alert.alert("There is something wrong!", err.message);
+  }
+}
