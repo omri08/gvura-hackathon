5238880a8929de19aaa4636b1f9be1ec06140113
omri08 <omrimalka08@gmail.com>
2021-03-30 16:27:52 +0300

    Added inital firebase function

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import apiKeys from './config/key';
+import { registration } from './services/firebase';
 
 export default function App() {
+  if (!firebase.apps.length) {
+    console.log('Connected with Firebase');
+    firebase.initializeApp(apiKeys);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello Hackathon</Text>
+      <Text
+        onPress={() =>
+          registration('omrimalka08@gmail.com', '123456', 'malka', 'omri')
+        }
+      >
+        Hello Hackathon
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
